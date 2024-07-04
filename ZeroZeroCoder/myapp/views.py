c5b50447eb6986bc2caa3e8eea743295f82ad5b6
krylovolegv <krylovolegv@yandex.ru>
2024-07-04 16:45:38 +0200

    add index.html

--- ZeroZeroCoder/myapp/views.py
+++ ZeroZeroCoder/myapp/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render  # Добавлен импорт функции render
 
 def index_view(request):
-    return HttpResponse("<h1>Главная страница</h1><p>Это главная страница</p>")
+    return render(request, 'myapp/index.html')
 
 def data_view(request):
     return HttpResponse("<h1>Страница Data</h1><p>Это страница для данных Data</p>")
