b724488a927e3ec864b7b040c995f93a9bf6e78a
krylovolegv <krylovolegv@yandex.ru>
2024-07-10 21:16:25 +0200

    add author colomn

--- ZeroZeroCoder/news/admin.py
+++ ZeroZeroCoder/news/admin.py
@@ -1,6 +1,9 @@
-
 from django.contrib import admin
 from .models import News_post
 
-admin.site.register(News_post)
-# Register your models here.
+class News_postAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_description', 'pub_date', 'author')
+    list_filter = ['pub_date', 'author']
+    search_fields = ['title', 'text']
+
+admin.site.register(News_post, News_postAdmin)
--- ZeroZeroCoder/news/models.py
+++ ZeroZeroCoder/news/models.py
@@ -1,14 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.conf import settings
 
 class News_post(models.Model):
     title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        default=1  # Предполагаем, что пользователь с id=1 существует
+    )
 
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
