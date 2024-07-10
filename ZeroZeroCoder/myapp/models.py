2732edd1018f49c8c1f11f45bc418c553a444883
krylovolegv <krylovolegv@yandex.ru>
2024-07-10 14:10:31 +0200

    add text 4 models.py

--- ZeroZeroCoder/myapp/models.py
+++ ZeroZeroCoder/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Полный текст новости')
+    pub_date = models.DateTimeField('Дата публикации') #вводит дату и ВРЕМЯ
 # Create your models here.
